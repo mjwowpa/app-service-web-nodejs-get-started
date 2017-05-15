76f81374dcef349a1a4e3c3a07973dfdf5b4784b
t-paarri <t-paarri@MININT-HESS1EN>
2017-05-15 14:28:44 +0200

    Title Opt

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NTO' });
+  res.render('index', { title: 'NTO BackEnd Hybrid POC' });
 });
 
 module.exports = router;
